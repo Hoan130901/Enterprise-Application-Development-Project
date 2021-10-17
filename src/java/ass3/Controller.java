a7b5192d7ac2e9965f0a9bf713138347e6f00f78
hoannguyen121309 <hoannguyen121309@gmail.com>
2021-10-17 19:38:20 +1100

    front code

---
+++ src/java/ass3/Controller.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass3;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author ilove
+ */
+@Named(value = "controller")
+@Dependent
+public class Controller {
+
+    /**
+     * Creates a new instance of Controller
+     */
+    public Controller() {
+    }
+    
+}
